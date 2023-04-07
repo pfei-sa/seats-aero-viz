6acdf30f522709386dbf5a2b5101f7633474b097
Peng Fei <4657356+pfei-sa@users.noreply.github.com>
2023-04-07 00:29:54 +0000

    Adds city and country expansion

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timedelta
-from typing import List, Tuple
+from typing import Dict, List, Tuple
+from itertools import product
 
 import streamlit as st
 import humanize
 import altair as alt
+import airportsdata
 
 from src.api import Availability, PARTNERS
 from src.plot import get_route_df
+from src.airport import city_expansion_dict, country_expansion_dict
 from datetime import datetime as time
 
 st.set_page_config(
@@ -41,14 +44,11 @@ for a in availabilities:
             f.strip() for f in a.airlines(fare).split(",") if f.strip() != ""
         )
 
-if "route" not in st.session_state:
-    st.session_state.route = "JFK -> HND -> BKK -> NRT, HND -> JFK"
-
 route = st.text_input(
-    "Route", max_chars=100, key="route"
+    "Route", "JFK -> HND -> BKK -> NRT, HND -> JFK", max_chars=300, key="route"
 ).upper()
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns([3, 3, 2])
 
 with col1:
     airlines = st.multiselect(
@@ -62,20 +62,50 @@ with col2:
         all_fares,
     )
 
+with col3:
+    expand_country = st.checkbox("Expand country", value=True)
+    expand_city = st.checkbox("Expand city", value=True)
 
-def canonicalize_route(route: str) -> List[Tuple[str, str]]:
+
+def canonicalize_route(
+    route: str, expand_country: bool = False, expand_city: bool = False
+) -> List[Tuple[str, str]]:
     route = route.replace(" ", "")
     segs = route.split(",")
     res = []
     for seg in segs:
         stops = seg.split("->")
         res.extend([(org, dest) for org, dest in zip(stops[:-1], stops[1:])])
+    return expand_route(res, expand_country, expand_city)
+
+
+def expand_route(
+    route: List[Tuple[str, str]], expand_country: bool, expand_city: bool
+) -> List[Tuple[str, str]]:
+    res = []
+    for org, dest in route:
+        res.extend(
+            product(
+                expand_code(org, expand_country, expand_city),
+                expand_code(dest, expand_country, expand_city),
+            )
+        )
     return res
+
+
+def expand_code(code: str, expand_country: bool, expand_city: bool) -> List[str]:
+    if expand_country and code in country_expansion_dict():
+        return country_expansion_dict()[code]
+    if expand_city and code in city_expansion_dict():
+        return city_expansion_dict()[code]
+    return [code]
 
 
 time_since_cache = time.now() - cache_freshness
 
-route_df = get_route_df(availabilities, canonicalize_route(route), airlines, fares)
+canonicalized_route = canonicalize_route(route, expand_city, expand_country)
+
+route_df = get_route_df(availabilities, canonicalized_route, airlines, fares)
 
 chart = (
     alt.Chart(route_df)
@@ -104,7 +134,7 @@ chart = (
         tooltip=["airlines", "fare", "date", "freshness"],
         row=alt.Row(
             "route",
-            sort=[f"{org} -> {dest}" for org, dest in canonicalize_route(route)],
+            sort=[f"{org} -> {dest}" for org, dest in canonicalized_route],
             header=alt.Header(labelAngle=0, labelAlign="left", labelFontSize=14),
             title=None,
         ),
@@ -118,7 +148,8 @@ st.altair_chart(
     theme=None,
 )
 
-for org, dest in canonicalize_route(route):
-    seg_str = f"{org} -> {dest}"
-    if seg_str not in route_df["route"].unique():
-        st.warning(f"No availability found for {seg_str}")
+with st.expander("Route without availability"):
+    for org, dest in canonicalized_route:
+        seg_str = f"{org} -> {dest}"
+        if seg_str not in route_df["route"].unique():
+            st.warning(f"No availability found for {seg_str}")
---
+++ src/airport.py
@@ -0,0 +1,582 @@
+
+'''
+**City Code Directory (CCD) – Multi Airport Cities list**
+
+.. list-table::
+   :header-rows: 1
+
+   * - Country
+     - City Code
+     - City Name
+     - Airport Code
+     - Airport Name
+   * - AE
+     - DXB
+     - Dubai
+     - DWC
+     - Al Maktoum Intl
+   * - AE
+     - DXB
+     - Dubai
+     - DXB
+     - International
+   * - AR
+     - BUE
+     - Buenos Aires
+     - AEP
+     - Jorge Newbery
+   * - AR
+     - BUE
+     - Buenos Aires
+     - EZE
+     - Ministro Pistarini
+   * - AU
+     - MEL
+     - Melbourne
+     - AVV
+     - Avalon
+   * - AU
+     - MEL
+     - Melbourne
+     - MEL
+     - Melbourne Airport
+   * - BE
+     - BRU
+     - Brussels
+     - BRU
+     - Brussels Airport
+   * - BE
+     - BRU
+     - Brussels
+     - CRL
+     - Brussels S. Charleroi
+   * - BR
+     - BHZ
+     - Belo Horizonte
+     - CNF
+     - Tancredo Neves Intl
+   * - BR
+     - BHZ
+     - Belo Horizonte
+     - PLU
+     - Pampulha
+   * - BR
+     - RIO
+     - Rio de Janeiro
+     - GIG
+     - Galeao-A.C.Jobim Intl
+   * - BR
+     - RIO
+     - Rio de Janeiro
+     - SDU
+     - Santos Dumont
+   * - BR
+     - SAO
+     - Sao Paulo
+     - CGH
+     - Congonhas
+   * - BR
+     - SAO
+     - Sao Paulo
+     - GRU
+     - Guarulhos Intl
+   * - BR
+     - SAO
+     - Sao Paulo
+     - VCP
+     - Viracopos-Campinas In
+   * - CA
+     - YTO
+     - Toronto
+     - YTZ
+     - Billy Bishop City A/P
+   * - CA
+     - YTO
+     - Toronto
+     - YYZ
+     - Lester B. Pearson Int
+   * - CN
+     - BJS
+     - Beijing
+     - PEK
+     - Capital Intl
+   * - CN
+     - BJS
+     - Beijing
+     - PKX
+     - Daxing Intl.
+   * - CN
+     - SHA
+     - Shanghai
+     - PVG
+     - Pudong Intl
+   * - CN
+     - SHA
+     - Shanghai
+     - SHA
+     - Hongqiao Intl
+   * - ES
+     - TCI
+     - Tenerife
+     - TFN
+     - Tenerife-Norte
+   * - ES
+     - TCI
+     - Tenerife
+     - TFS
+     - Tenerife-Sur
+   * - FR
+     - PAR
+     - Paris
+     - CDG
+     - Charles de Gaulle
+   * - FR
+     - PAR
+     - Paris
+     - ORY
+     - Orly
+   * - GB
+     - BFS
+     - Belfast
+     - BFS
+     - International
+   * - GB
+     - BFS
+     - Belfast
+     - BHD
+     - George Best City Apt
+   * - GB
+     - LON
+     - London
+     - LCY
+     - City Airport
+   * - GB
+     - LON
+     - London
+     - LGW
+     - Gatwick
+   * - GB
+     - LON
+     - London
+     - LHR
+     - Heathrow
+   * - GB
+     - LON
+     - London
+     - LTN
+     - Luton
+   * - GB
+     - LON
+     - London
+     - STN
+     - Stansted
+   * - ID
+     - JKT
+     - Jakarta
+     - CGK
+     - Soekarno-Hatta Intl
+   * - ID
+     - JKT
+     - Jakarta
+     - HLP
+     - Halim Perdanakusuma
+   * - ID
+     - JOG
+     - Yogyakarta
+     - JOG
+     - Adisutjipto
+   * - ID
+     - JOG
+     - Yogyakarta
+     - YIA
+     - New Yogyakarta Int.
+   * - IR
+     - THR
+     - Tehran
+     - IKA
+     - Imam Khomeini Intl
+   * - IR
+     - THR
+     - Tehran
+     - THR
+     - Mehrabad Intl
+   * - IS
+     - REK
+     - Reykjavik
+     - KEF
+     - Keflavik International
+   * - IS
+     - REK
+     - Reykjavik
+     - RKV
+     - Reykjavik Domestic
+   * - IT
+     - MIL
+     - Milan
+     - BGY
+     - Bergamo/Orio al Serio
+   * - IT
+     - MIL
+     - Milan
+     - LIN
+     - Linate
+   * - IT
+     - MIL
+     - Milan
+     - MXP
+     - Malpensa
+   * - IT
+     - ROM
+     - Rome
+     - CIA
+     - Ciampino
+   * - IT
+     - ROM
+     - Rome
+     - FCO
+     - Fiumicino
+   * - JP
+     - NGO
+     - Nagoya
+     - NGO
+     - Chubu Centrair International
+   * - JP
+     - NGO
+     - Nagoya
+     - NKM
+     - Nagoya (Komaki)
+   * - JP
+     - OSA
+     - Osaka
+     - ITM
+     - Osaka Intl (Itami)
+   * - JP
+     - OSA
+     - Osaka
+     - KIX
+     - Kansai International
+   * - JP
+     - OSA
+     - Osaka
+     - UKB
+     - Kobe
+   * - JP
+     - SPK
+     - Sapporo
+     - CTS
+     - New Chitose
+   * - JP
+     - SPK
+     - Sapporo
+     - OKD
+     - Okadama
+   * - JP
+     - TYO
+     - Tokyo
+     - HND
+     - Tokyo Intl (Haneda)
+   * - JP
+     - TYO
+     - Tokyo
+     - NRT
+     - Narita Intl
+   * - KR
+     - SEL
+     - Seoul
+     - GMP
+     - Gimpo International
+   * - KR
+     - SEL
+     - Seoul
+     - ICN
+     - Incheon International
+   * - SL
+     - SLU
+     - St Lucia
+     - SLU
+     - George F.L. Charles
+   * - SL
+     - SLU
+     - St Lucia
+     - UVF
+     - Hewanorra Int’l
+   * - NO
+     - OSL
+     - Oslo
+     - OSL
+     - Gardermoen
+   * - NO
+     - OSL
+     - Oslo
+     - TRF
+     - Sandefjord-Torp
+   * - RU
+     - MOW
+     - Moscow
+     - DME
+     - Domodedovo
+   * - RU
+     - MOW
+     - Moscow
+     - SVO
+     - Sheremetyevo
+   * - RU
+     - MOW
+     - Moscow
+     - VKO
+     - Vnukovo
+   * - SE
+     - STO
+     - Stockholm
+     - ARN
+     - Arlanda
+   * - SE
+     - STO
+     - Stockholm
+     - BMA
+     - Bromma
+   * - SN
+     - DKR
+     - Dakar
+     - DKR
+     - Leopold Sedar Senghor
+   * - SN
+     - DKR
+     - Dakar
+     - DSS
+     - Blaise Diagne Intl
+   * - TH
+     - BKK
+     - Bangkok
+     - BKK
+     - Suvarnabhumi Airport
+   * - TH
+     - BKK
+     - Bangkok
+     - DMK
+     - Don Mueang Int'l
+   * - TR
+     - ANK
+     - Ankara
+     - ANK
+     - Etimesgut
+   * - TR
+     - ANK
+     - Ankara
+     - ESB
+     - Esenboga
+   * - TR
+     - IST
+     - Istanbul
+     - ISL
+     - Ataturk
+   * - TR
+     - IST
+     - Istanbul
+     - IST
+     - Istanbul Airport
+   * - TR
+     - IST
+     - Istanbul
+     - SAW
+     - Sabiha Gokcen
+   * - TW
+     - TPE
+     - Taipei
+     - TPE
+     - Taoyuan International Airport
+   * - TW
+     - TPE
+     - Taipei
+     - TSA
+     - Songshan
+   * - UA
+     - IEV
+     - Kyiv
+     - IEV
+     - Kyiv International Airport
+   * - UA
+     - IEV
+     - Kyiv
+     - KBP
+     - Boryspil Intl
+   * - US
+     - CHI
+     - Chicago
+     - MDW
+     - Midway International
+   * - US
+     - CHI
+     - Chicago
+     - ORD
+     - O'Hare International
+   * - US
+     - DFW
+     - Dallas
+     - DAL
+     - Love Field
+   * - US
+     - DFW
+     - Dallas
+     - DFW
+     - Dallas/Ft Worth Intl
+   * - US
+     - HOU
+     - Houston
+     - HOU
+     - William P Hobby Airport
+   * - US
+     - HOU
+     - Houston
+     - IAH
+     - George Bush Intercontinental
+   * - US
+     - NYC
+     - New York
+     - JFK
+     - John F Kennedy Intl
+   * - US
+     - NYC
+     - New York
+     - LGA
+     - LaGuardia
+   * - US
+     - WAS
+     - Washington
+     - DCA
+     - Ronald Reagan National
+   * - US
+     - WAS
+     - Washington
+     - IAD
+     - Dulles Intl
+   * - ZA
+     - JNB
+     - Johannesburg
+     - HLA
+     - Lanseria International
+   * - ZA
+     - JNB
+     - Johannesburg
+     - JNB
+     - \O. R. Tambo International Airport
+'''
+
+import functools
+from typing import List, Dict
+import airportsdata
+
+
+CITY_TO_IATA = [
+    ["DXB", "DWC"],
+    ["DXB", "DXB"],
+    ["BUE", "AEP"],
+    ["BUE", "EZE"],
+    ["MEL", "AVV"],
+    ["MEL", "MEL"],
+    ["BRU", "BRU"],
+    ["BRU", "CRL"],
+    ["BHZ", "CNF"],
+    ["BHZ", "PLU"],
+    ["RIO", "GIG"],
+    ["RIO", "SDU"],
+    ["SAO", "CGH"],
+    ["SAO", "GRU"],
+    ["SAO", "VCP"],
+    ["YTO", "YTZ"],
+    ["YTO", "YYZ"],
+    ["BJS", "PEK"],
+    ["BJS", "PKX"],
+    ["SHA", "PVG"],
+    ["SHA", "SHA"],
+    ["TCI", "TFN"],
+    ["TCI", "TFS"],
+    ["PAR", "CDG"],
+    ["PAR", "ORY"],
+    ["BFS", "BFS"],
+    ["BFS", "BHD"],
+    ["LON", "LCY"],
+    ["LON", "LGW"],
+    ["LON", "LHR"],
+    ["LON", "LTN"],
+    ["LON", "STN"],
+    ["JKT", "CGK"],
+    ["JKT", "HLP"],
+    ["JOG", "JOG"],
+    ["JOG", "YIA"],
+    ["THR", "IKA"],
+    ["THR", "THR"],
+    ["REK", "KEF"],
+    ["REK", "RKV"],
+    ["MIL", "BGY"],
+    ["MIL", "LIN"],
+    ["MIL", "MXP"],
+    ["ROM", "CIA"],
+    ["ROM", "FCO"],
+    ["NGO", "NGO"],
+    ["NGO", "NKM"],
+    ["OSA", "ITM"],
+    ["OSA", "KIX"],
+    ["OSA", "UKB"],
+    ["SPK", "CTS"],
+    ["SPK", "OKD"],
+    ["TYO", "HND"],
+    ["TYO", "NRT"],
+    ["SEL", "GMP"],
+    ["SEL", "ICN"],
+    ["SLU", "SLU"],
+    ["SLU", "UVF"],
+    ["OSL", "OSL"],
+    ["OSL", "TRF"],
+    ["MOW", "DME"],
+    ["MOW", "SVO"],
+    ["MOW", "VKO"],
+    ["STO", "ARN"],
+    ["STO", "BMA"],
+    ["DKR", "DKR"],
+    ["DKR", "DSS"],
+    ["BKK", "BKK"],
+    ["BKK", "DMK"],
+    ["ANK", "ANK"],
+    ["ANK", "ESB"],
+    ["IST", "ISL"],
+    ["IST", "IST"],
+    ["IST", "SAW"],
+    ["TPE", "TPE"],
+    ["TPE", "TSA"],
+    ["IEV", "IEV"],
+    ["IEV", "KBP"],
+    ["CHI", "MDW"],
+    ["CHI", "ORD"],
+    ["DFW", "DAL"],
+    ["DFW", "DFW"],
+    ["HOU", "HOU"],
+    ["HOU", "IAH"],
+    ["NYC", "JFK"],
+    ["NYC", "LGA"],
+    ["WAS", "DCA"],
+    ["WAS", "IAD"],
+    ["JNB", "HLA"],
+    ["JNB", "JNB"],
+]
+
+@functools.cache
+def city_expansion_dict() -> Dict[str, List[str]]:
+    """Return a dictionary mapping city names to a list of IATA codes."""
+    city_to_iata = {}
+    for city, airport in CITY_TO_IATA:
+        if city not in city_to_iata:
+            city_to_iata[city] = []
+        city_to_iata[city].append(airport)
+    return city_to_iata
+
+@functools.cache
+def country_expansion_dict() -> Dict[str, List[str]]:
+    """Return a dictionary mapping country names to a list of IATA codes."""
+    country_to_iata = {}
+    for code, airport in airportsdata.load("IATA").items():
+        country = airport["country"]
+        if country not in country_to_iata:
+            country_to_iata[country] = []
+        country_to_iata[country].append(code)
+    return country_to_iata
